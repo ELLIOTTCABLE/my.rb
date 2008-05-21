c86f33e2fb7a136f6d7ab0cb4dab9dbf3fde837f
elliottcable <git@elliottcable.name>
2008-05-21 10:56:24 -0800

    Cool! I think it's actually useful now!

--- lib/my.rb
+++ lib/my.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require 'myrb/core_ext/file'
 
 # Allows calling MyRB as +my.rb+ or +My.RB+, because I'm a lazy, semantic fucker - heh
@@ -14,12 +15,28 @@ end
 
 # Aaaaand we welcome to the stage, Mr. My.RB! *clapclapclap*
 class MyRB
+  Directory = File.join '~', '.my.rb'
+  ConfigFile = File.join Directory, 'config'
+  @@config = nil
   
   # ===============
   # = As a module =
   # ===============
   def self.loaded
-    $LOADED_FEATURES.select {|f| f =~ %r|^my.rb/|}.map {|f| f.gsub(%r|^my.rb/|, '')}
+    $LOADED_FEATURES.select {|f| f =~ %r|^my.rb/|}.map {|f| f.gsub(%r!(^my\.rb/|(\.my)?\.rb$)!, '')}
+  end
+  
+  def self.config
+    raise RuntimeError, '** You need to run `my.rb init` before you can use my.rb!' unless
+      File.directory?(File.expand_path(Directory)) && File.file?(File.expand_path(ConfigFile))
+    return @@config unless @@config.nil?
+    @@config = ::YAML::load_file File.expand_path(ConfigFile)
+  end
+  
+  def self.save_config
+    File.open File.expand_path(Config), File::WRONLY|File::TRUNC|File::CREAT do |file|
+      ::YAML::dump my.rb.config
+    end
   end
   
   # ==================
@@ -39,7 +56,6 @@ class MyRB
   attr_accessor :categories
   
   def initialize *args, &block
-    path = 
     args = args.inject(Hash.new) {|a,o| raise ArgumentError unless a.class == Hash; o.merge a }
     
     Variables.each do |arg|
@@ -60,9 +76,8 @@ class MyRB
   def self.new_from path, *args, &block
     args = args.inject(Hash.new) {|a,o| raise ArgumentError unless a.class == Hash; o.merge a }
     path = File.expand_path path
-    categories = path.gsub(%r|^/.*/my\.rb/|, '')
-    categories = File.split_all categories
-    name = categories.pop.gsub(/\.my\.rb$/, '')
+    categories = path.match(%r|^/.*/my\.rb/|) ? File.split_all(path.gsub(%r|^/.*/my\.rb/|, '')) : []
+    name = File.basename(path).gsub(/(\.my)?\.rb$/, '')
     
     description, content = my.rb.parse path
     my.rb.new(
@@ -88,19 +103,26 @@ class MyRB
   end
   
   # Saves a my.rb out to a .my.rb file in the my.rb dir
-  def save
-    raise '** You need to run `my.rb init` before you can use my.rb!'
-    
-    path = File.expand_path File.join('~', @categories, "#{@name}.my.rb")
+  def save *args
+    args = args.inject(Hash.new) {|a,o| raise ArgumentError unless a.class == Hash; o.merge a }
+    path = File.expand_path File.join(my.rb.config[:directory], 'my.rb', @categories, "#{@name}.rb")
+    if File.file? path
+      raise "** my.rb #{@name} exists at [#{path}] already! Pass :overwrite => true if you wish to overwrite it."
+    end
     File.open path, File::RDWR|File::TRUNC|File::CREAT do |file|
       file.puts @description.gsub /^/, "# "
       file << "\n"
       file.puts @content
       file.close
     end
+    self
   end
   
 end
 
 # One last request... before I die... *gaaasp*...
-$LOAD_PATH.unshift File.join(File.expand_path('~'), '.myrb', 'snippets')
+begin
+  $LOAD_PATH.unshift File.join(my.rb.config[:directory])
+rescue RuntimeError => e
+  puts e
+end
--- lib/myrb.rake
+++ lib/myrb.rake
@@ -1,9 +1,35 @@
 ( $:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')) ).uniq!
 require 'my.rb'
+require 'fileutils'
+include FileUtils
 
 desc 'Prepares my.rb for use'
-task :init do
-  
+task :init => [:config] do
+  config = YAML.load_file(File.expand_path(my.rb::ConfigFile))
+  mkdir_p File.join(File.expand_path(config[:directory]), 'my.rb')
+  puts "** my.rb will store snippets in [#{File.join(config[:directory], 'my.rb')}]"
+  puts '** my.rb is ready to be used!'
+end
+
+task :config do
+  mkdir_p File.expand_path(my.rb::Directory)
+  next if File.file? File.expand_path(my.rb::ConfigFile)
+  File.open File.expand_path(my.rb::ConfigFile), File::WRONLY|File::TRUNC|File::CREAT do |file|
+    file.puts '# This config file must remain at ~/.my.rb/config, but you can change'
+    file.puts '# the global snippets location with the :directory: declaration below.'
+    file << "\n"
+    
+    file.puts({
+      :name       => nil,
+      :email      => nil,
+      :directory  => File.join('~', '.my.rb', 'snippets')
+    }.to_yaml)
+  end
+  puts "** Config file at [#{my.rb::ConfigFile}] created. Please edit it with the"
+  puts "** correct information, and then run `my.rb init` again."
+  puts "** Attempting to open the config file with $VISUAL (#{ENV['VISUAL']})..."
+  system "#{ENV['VISUAL']} #{my.rb::ConfigFile}"
+  exit
 end
 
 task :default do
